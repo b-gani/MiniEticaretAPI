282872d456ec3ce5c8703bc87788397e28b944b8
b-gani <burakgani37@gmail.com>
2022-07-28 00:47:12 +0300

    ServiceRegistration düzenleme

--- Infrastructure/EticaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/EticaretAPI.Persistence/ServiceRegistration.cs
@@ -2,15 +2,16 @@
 using EticaretAPI.Persistence.Contexts;
 using EticaretAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EticaretAPI.Persistence
 {
     public static class ServiceRegistration
     {
-        public static void AddPersistenceServices(this IServiceCollection services)
+        public static void AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
         {
-            services.AddDbContext<EticaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<EticaretAPIDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
             services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
             services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
             services.AddScoped<IOrderReadRepository, OrderReadRepository>();
