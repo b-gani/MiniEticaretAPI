29833bceda18b20e4ceb930f082d9e2bb0622ea9
b-gani <burakgani37@gmail.com>
2022-07-31 19:54:34 +0300

    SaveChangeAsync Interceptor

--- Infrastructure/EticaretAPI.Persistence/Contexts/EticaretAPIDbContext.cs
+++ Infrastructure/EticaretAPI.Persistence/Contexts/EticaretAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EticaretAPI.Domain.Entities;
+using EticaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace EticaretAPI.Persistence.Contexts
@@ -11,5 +12,20 @@ namespace EticaretAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                var _ = data.State switch
+                {
+                    EntityState.Modified => data.Entity.UpdatedDate=DateTime.UtcNow,
+                    EntityState.Added => data.Entity.CreatedDate=DateTime.UtcNow
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- Presentation/EticaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/EticaretAPI.API/Controllers/ProductsController.cs
@@ -7,36 +7,19 @@ namespace EticaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductWriteRepository _productWriteRepository;
-        private readonly IProductReadRepository _productReadRepository;
+        
 
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        public ProductsController()
         {
-            _productReadRepository = productReadRepository;
-            _productWriteRepository = productWriteRepository;
+             
         }
 
         [HttpGet]
-        public async Task Get()
+        public Task Get()
         {
-            //await _productWriteRepository.AddRangeAsync(new()
-            //{
-            //    new(){ Id=Guid.NewGuid(),Name="Product 1", Price=100, CreatedDate=DateTime.UtcNow, Stock=10 },
-            //    new(){ Id=Guid.NewGuid(),Name="Product 2", Price=200, CreatedDate=DateTime.UtcNow, Stock=20 },
-            //    new(){ Id=Guid.NewGuid(),Name="Product 3", Price=300, CreatedDate=DateTime.UtcNow, Stock=30 },
-            //    new(){ Id=Guid.NewGuid(),Name="Product 4", Price=400, CreatedDate=DateTime.UtcNow, Stock=40 },
-            //});
-            //await _productWriteRepository.SaveAsync();
+            throw new NotImplementedException();
+            
+        }
 
-            var p = await _productReadRepository.GetByIdAsync("3a2d70e8-401d-4e8d-a127-46c167615e66",false);
-            p.Name = "Mehmet";
-            await _productWriteRepository.SaveAsync();
-        }
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
-        {
-            var product = await _productReadRepository.GetByIdAsync(id);
-            return Ok(product);
-        }
     }
 }
